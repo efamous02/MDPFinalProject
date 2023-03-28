2413febb0f89fd6d03d482c177cdfc61281c8bba
Emma Famous <emma@famousfamily.com>
2023-03-28 13:57:54 -0400

    Initial Commit

---
+++ MDPFinalProject/MDPFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDPFinalProjectApp.swift
+//  MDPFinalProject
+//
+//  Created by Emma Famous on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MDPFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
