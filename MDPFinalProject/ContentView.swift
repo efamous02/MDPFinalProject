7c669bc5695b04e6d68a032e25ae2dbec4aa5f1f
Emma Famous <emma@famousfamily.com>
2023-05-11 19:07:00 -0400

    Design Overhaul. Simplified design into 2 api's

--- MDPFinalProject/ContentView.swift
+++ MDPFinalProject/ContentView.swift
@@ -6,7 +6,13 @@
 //
 
 import SwiftUI
-
+extension Color {
+    static let ui = Color.UI()
+    
+    struct UI {
+         let Primary = Color("Primary")
+    }
+}
 //JSON STRUCTURES
 //GeoCode Info
 struct geoInfo: Decodable{
@@ -19,38 +25,27 @@ struct geoInfo: Decodable{
 struct currentWeatherData: Decodable {
     let weather: [weatherData]
     let main: mainData
+    let sys: sysData
+    let timezone: Int
+    let name: String
     
 }
 struct mainData: Decodable{
     let temp: Double
+    let feels_like: Double
+    let temp_min: Double
+    let temp_max: Double
+    let humidity: Double
 }
 struct weatherData: Decodable {
     let main: String
     let description: String
     let icon: String
 }
-
-//Next 5 Days
-/*struct forcastDayData: Decodable{
-    let main: [mainForcast]
-    let weather: [weatherForcast]
+struct sysData: Decodable{
+    let sunrise: Int
+    let sunset: Int
 }
-*/
-struct mainForcast{
-    let temp_min: Double
-    let temp_max: Double
-}
-struct weatherForcast{
-    let icon: String
-}
-struct windForcast{
-    let speed: Double
-    let gust: Double
-}
-struct rainForcast{
-    let oneHour: Double
-}
-
 
 struct ContentView: View {
     @State private var Location = ""
@@ -58,122 +53,105 @@ struct ContentView: View {
     
     @State private var tempInFahrenheit = "Na"
     @State private var currentWeatherString = ""
-    @State private var weatherIcon = "magnifyingglass.circle"
+    @State private var weatherIcon = "sun.max"
+    @State private var feelsLike = "0"
+
+    @State private var tempMin = 56.0
+    @State private var tempMax = 72.0
     
-    //TODO:REMOVE
-    //@State private var debugOutput = ":D"
-    
-    @State private var day1Data1 = ""
-    @State private var day2Data1 = ""
-    @State private var day3Data1 = ""
-    @State private var day4Data1 = ""
+    @State private var Sunrise = "9:00"
+    @State private var Sunset = "9:00"
 
 
-    @State private var day1Data2 = ""
-    @State private var day2Data2 = ""
-    @State private var day3Data2 = ""
-    @State private var day4Data2 = ""
+    @State private var weatherColor = Color(red: 242/255, green: 206/255, blue: 48/255)
     
     var body: some View {
-        VStack {
-           
-            HStack(alignment: .top){
-                TextField(CurrentLocation,text: $Location)
-                Button(action: {
-                    Task{
-                         await fetchGeoData()
-                    }
-                }, label: {
-                    Image(systemName: "magnifyingglass.circle")
-                })
-            }//end Hstack
-            .border(.secondary)
-            .padding(.horizontal)
-            
-            Spacer()
-            Text(CurrentLocation)
-            HStack{
-                //Text(debugOutput)
-
-                Text(tempInFahrenheit.prefix(2)+"°")
-                    .font(.largeTitle)
-                    .padding()
-                Image(systemName: weatherIcon)
-
-            }//End Hstack
-            Text(currentWeatherString)
-            
-            
-            Spacer()
-
-            Text("Next 4 Days")
-            HStack{
-                Button(action: {
-                    Task{
-                        setDataToWeather()
-                    }
-                }, label: {
-                    Text("Weather")
-                })
-                Spacer()
-                Button(action: {
-                    Task{
-                        setDataToRain()
-                    }
-                }, label: {
-                    Text("Rain")
-                })
-                Spacer()
-                Button(action: {
-                    Task{
-                        setDataToWind()
-                    }
-                }, label: {
-                    Text("Wind")
-                })
-            }// End Hstack
-            .padding()
-            HStack{
-                VStack{
-                    Image(systemName: "sun.max.fill")
-                    Image(systemName: "cloud.fill")
-                    Image(systemName: "cloud.drizzle.fill")
-                    Image(systemName: "cloud.rain.fill")
-                    Image(systemName: "cloud.snow.fill")
+            VStack {
+                HStack(alignment: .top){
+                    TextField(CurrentLocation,text: $Location)
+                    Button(action: {
+                        Task{
+                             await fetchGeoData()
+                        }
+                    }, label: {
+                        Image(systemName: "magnifyingglass.circle")
+                    })
+                }//end Hstack
+                .border(.secondary)
+                .padding(.horizontal)
+                .background(weatherColor)
+                
+                ZStack{
+                    /*
+                    Rectangle()
+                        .fill(.white)
+                        .border(.black, width:2)
+                        .frame(width: 600, height: 200)
+                     */
+                    VStack{
+                        Text(Location)
+                            .font(.title2)
+                            .bold()
+                            .italic()
+                        Text(CurrentLocation)
+                            .font(.subheadline)
+                            .italic()
+                        HStack{
+                            ZStack{
+                                Circle()
+                                    .frame(width: 80, height:80)
+                                    .foregroundColor(weatherColor)
+                                    .shadow(color: .gray, radius: 1.0, x: 0, y: 1.0)
+                                Text(tempInFahrenheit.prefix(2)+"°")
+                                    .font(.largeTitle)
+                                    .padding()
+                            }
+                            
+                            Image(systemName: weatherIcon)
+                                .imageScale(.large)
+                            
+                        }//End Hstack
+                        Text("Feels like: " + feelsLike.prefix(2) + "°")
+                    }//End VStack
+                    .padding(50)
+                }//End ZStack
+                HStack{
+                    Text(String(tempMin).prefix(2) + "°")
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .frame(width: (tempMax - tempMin)*8, height: 10)
+                        .foregroundColor(weatherColor)
+                        .shadow(color: .gray, radius: 1.0, x: 0, y: 1.0)
+                    Text(String(tempMax).prefix(2) + "°")
                 }
-                Spacer()
-                VStack{
-                    Text("20 - 100")
-                    Text("82 - 86")
-                    Text("90 - 99")
-                    Text("83 - 90 ")
-                    Text("80 - 20")
+                .font(.title3)
+                .padding()
+                
+                HStack{
+                    Image(systemName: "sunrise")
+                        .imageScale(.large)
+                        .padding()
+                        .foregroundColor(weatherColor)
+                    Text(Sunrise)
+                    Image(systemName: "sunset")
+                        .imageScale(.large)
+                        .padding()
+                        .foregroundColor(weatherColor)
+                    Text(Sunset)
                 }
+                .padding(.trailing)
+
+               
+
                 Spacer()
-            }//End Hstack
+            }//End Vstack
             .padding()
-            .font(.title3)
-            Spacer()
-
-
-        }//End Vstack
-        .padding()
-        .task {
-            await fetchWeatherData(lat: 44.34, long: 10.99)
-        }
-        
-
-        
+       
     }
-    func setDataToWeather(){
-        
+   
+    func kelvinToFahrenheit(input: Double)-> Double{
+        return 1.8*(input-273)+32
     }
-    func setDataToRain(){
-        
-    }
-    func setDataToWind(){
-        
-    }
+    
     func fetchWeatherData(lat:Double, long:Double) async {
         let apiKey = "3b22829df4345b0039be06a55eccf9ee"
 
@@ -183,7 +161,6 @@ struct ContentView: View {
                     print("Invalid URL")
                     return
         }
-        
         do{
             let (data, _) = try await URLSession.shared.data(from: url)
             
@@ -192,48 +169,65 @@ struct ContentView: View {
             currentWeatherString = output.weather[0].main
             //temp is by default in kelvin
             let tempInKelvin = output.main.temp
-            tempInFahrenheit = String(1.8*(tempInKelvin-273)+32)
+            tempInFahrenheit = String(kelvinToFahrenheit(input: tempInKelvin))
             weatherIcon = findWeatherIcon(weatherIconNum: output.weather[0].icon)
             
+            feelsLike = String(kelvinToFahrenheit(input: output.main.feels_like))
+            
+            tempMin = kelvinToFahrenheit(input: output.main.temp_min)
+            tempMax = kelvinToFahrenheit(input: output.main.temp_max)
+            
+            let sunriseTime = Date(timeIntervalSince1970: TimeInterval(output.sys.sunrise ))
+            let sunsetTime = Date(timeIntervalSince1970: TimeInterval(output.sys.sunset ))
+            
+            Sunrise = sunriseTime.formatted()
+            Sunset = sunsetTime.formatted()
+
+            
         }catch{
+            CurrentLocation = "Error"
             print("error retreiving data")
             
         }
-        
-        //Next 5 data
-        //https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid={API key}
-        let forcastAPICall = "https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=" + String(lat) + "&lon=" + String(long) + "&appid=" + apiKey
-        guard let urlF = URL(string: forcastAPICall) else{
-            print("Invalid URL")
-            return
-        }
     }//end fetchWeatherData
-    
+
     func findWeatherIcon(weatherIconNum: String) -> String{
         //Sunny
         if(weatherIconNum == "01d" ){
+            weatherColor = Color(red: 242/255, green: 206/255, blue: 48/255)
             return "sun.max"
         }else if(weatherIconNum == "01n"){
+            weatherColor = Color(red: 144/255, green: 157/255, blue: 168/255)
             return "moon"
         }else if(weatherIconNum == "02d"){
+            weatherColor = Color(red: 242/255, green: 206/255, blue: 48/255)
             return "cloud.sun"
         }else if(weatherIconNum == "02n"){
+            weatherColor = Color(red: 148/255, green: 154/255, blue: 181/255)
             return "cloud.moon"
         }else if(weatherIconNum == "03d" || weatherIconNum == "03n"){
+            weatherColor = Color(red: 151/255, green: 154/255, blue: 163/255)
             return "cloud"
         }else if(weatherIconNum == "04d" || weatherIconNum == "04n"){
+            weatherColor = Color(red: 97/255, green: 98/255, blue: 102/255)
             return "smoke"
         }else if(weatherIconNum == "09d" || weatherIconNum == "09n"){
+            weatherColor = Color(red: 129/225, green: 145/225, blue: 209/225)
             return "cloud.drizzle"
         }else if(weatherIconNum == "10d" || weatherIconNum == "10n"){
+            weatherColor = Color(red: 82/255, green: 94/255, blue: 102/255)
             return "cloud.heavyrain"
         }else if(weatherIconNum ==  "11d" || weatherIconNum == "11n"){
+            weatherColor = Color(red: 98/255, green: 98/255, blue: 124/255)
             return "cloud.bolt"
         }else if(weatherIconNum == "13d" || weatherIconNum == "13n"){
+            weatherColor = Color(red: 186/255, green: 195/255, blue: 211/255)
             return "cloud.snow"
         }else if(weatherIconNum == "50d"){
+            weatherColor = Color(red: 204/255, green: 211/255, blue: 169/255)
             return "sun.haze"
         }else if (weatherIconNum == "50n"){
+            weatherColor = Color(red: 186/255, green: 195/255, blue: 211/255)
             return "moon.haze"
         }
         return "exclamationmark.triangle"
@@ -259,7 +253,7 @@ struct ContentView: View {
                 let long = Double(output[0].lon)
                 
                 await fetchWeatherData(lat: lat!, long: long!)
-
+                
             }
             else{
                 CurrentLocation = "Invalid Location"
@@ -267,7 +261,6 @@ struct ContentView: View {
 
 
         }catch{
-            CurrentLocation = "Error retrieving Location Info"
             print(error)
         }
 
@@ -280,4 +273,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
 
